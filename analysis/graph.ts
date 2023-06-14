495d07181019ff23a70616a0b392aca48a443274
snuup <hop@home.com>
2023-06-14 22:53:30 +0200

    pie done for now

--- analysis/graph.ts
+++ analysis/graph.ts
@@ -59,7 +59,7 @@ export class Graph {
     get linkcountsByType() { return this.links.countBy(n => n.type) }
 
     gettopdegrees(inludeInvestigatees = true) {
-        let tops = m.graph.nodes.sortBy(n => -n.degree).slice(0, 25)
+        let tops = m.graph.nodes.sortBy(n => -n.degree)//.slice(0, 25)
         let names = tops.map(n => n.nid)
 
         let missings = m.investigatees.filter(inv => !names.includes(inv)).map(this.getnode)

79c434470df7ec284d8b49247339e1f5e19cee8b
snuup <61933552+snuup@users.noreply.github.com>
2023-06-30 16:00:22 +0200

    FlowNode classes

--- comp/sankey.tsx
+++ comp/sankey.tsx
@@ -4,6 +4,29 @@ import { FishLink } from "../analysis/fishlink"
 import { jsx } from "../jmx-lib/core"
 import { mount } from "../utils/common"
 
+class SourceNode implements FlowNode {
+    id: string
+    constructor(id) { this.id = id }
+    get flowid() { return "s" + this.id }
+}
+
+class TargetNode implements FlowNode {
+    id: string
+    constructor(id) { this.id = id }
+    get flowid() { return "t" + this.id }
+}
+
+interface FlowNode {
+    get id(): string
+    get flowid(): string
+}
+
+class FlowLink {
+    constructor(public _source, public _target, public value) { }
+    get source() {return "s" + this._source }
+    get target() {return "t" + this._target }
+}
+
 export const SankeyForType = ({ links }: { links: FishLink[] }) => {
 
     let sourcetypes = links.map(l => l.target.type).distinctBy()
@@ -17,7 +40,7 @@ export const SankeyForType = ({ links }: { links: FishLink[] }) => {
 
     let linksbysource = links.groupBy(l => l.source.type)
 
-    let flownodes = alltypes.map(t => "s" + t).concat(alltypes.map(t => "t" + t))
+    let flownodes: FlowNode[] = alltypes.flatMap(t => [new SourceNode(t), new TargetNode(t)])
 
     let flowlinks: any[] = []
     for (let source in linksbysource) {
@@ -31,47 +54,39 @@ export const SankeyForType = ({ links }: { links: FishLink[] }) => {
         }
     }
 
-    mount({ sourcetypes, targettypes, linksbysource, alltypes, flowlinks })
-
-    const width = 600
+    const width = 500
     const height = 300
 
     function rund3(e: HTMLElement) {
         {
-            console.log("sankey")
-
-            const layout = d3s.sankey<any, any>()
-                .nodeId(d => d.data)
+            const layout = d3s.sankey<FlowNode, any>()
+                .nodeId(d => d.flowid)
                 .nodeWidth(15)
                 .nodePadding(20)
-                .extent([[0, 0], [width, height]]);
+                .extent([[1, 10], [width, height + 20]])
 
             let { nodes, links } = layout({
-                nodes: flownodes.map(d => ({ data: d })),
+                nodes: flownodes,
                 links: flowlinks
             })
-
-            mount({ layout })
 
             const svg = d3
                 .select(e)
                 .append("svg")
-                .attr("viewBox", [0, 0, width, height]);
-
-            //const { nodes, links } = layout(data);
+                .attr("viewBox", [0, 0, width, height])
 
             svg.append("g")
                 .attr("stroke", "#000")
                 .selectAll("rect")
                 .data(nodes)
                 .join("rect")
-                .attr("x", d => d.x0)
-                .attr("y", d => d.y0)
-                .attr("height", d => d.y1 - d.y0)
-                .attr("width", d => d.x1 - d.x0)
-                .attr("fill", "violet")
+                .attr("x", d => d.x0!)
+                .attr("y", d => d.y0!)
+                .attr("height", d => d.y1! - d.y0!)
+                .attr("width", d => d.x1! - d.x0!)
+                .attr("class", d => d.id)
                 .append("title")
-                .text(d => d.name);
+                .text(d => d.id)
 
             const link = svg.append("g")
                 .attr("fill", "none")
@@ -79,15 +94,15 @@ export const SankeyForType = ({ links }: { links: FishLink[] }) => {
                 .selectAll("g")
                 .data(links)
                 .join("g")
-                .style("mix-blend-mode", "multiply");
+                .style("mix-blend-mode", "multiply")
 
             link.append("path")
                 .attr("d", d3s.sankeyLinkHorizontal())
                 .attr("stroke", 'black')
-                .attr("stroke-width", d => Math.max(1, d.width));
+                .attr("stroke-width", d => Math.max(1, d.width))
 
             link.append("title")
-                .text(d => `${d.source.name} → ${d.target.name}\n${d.value}`);
+                .text(d => `${d.source.name} → ${d.target.name}\n${d.value}`)
 
             svg.append("g")
                 .attr("font-family", "sans-serif")
@@ -99,13 +114,9 @@ export const SankeyForType = ({ links }: { links: FishLink[] }) => {
                 .attr("y", d => (d.y1 + d.y0) / 2)
                 .attr("dy", "0.35em")
                 .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
-                .text(d => d.name);
+                .text(d => d.id)
         }
     }
 
-    return (
-        <div>
-            <div patch={rund3} />
-            <b>sankey</b>
-        </div>)
+    return <div patch={rund3} />
 }

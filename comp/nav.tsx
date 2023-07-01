96e3831668b08429717437aa57562f64ed447c15
snuup <hop@home.com>
2023-07-02 00:31:00 +0200

    suspect nodes

--- comp/nav.tsx
+++ comp/nav.tsx
@@ -3,9 +3,14 @@ import { jsx } from "../jmx-lib/core"
 import { NodeView } from "./node-view"
 
 export const Navigation = () => {
+
+    let invs = m.investigatees.map(m.graph.getnode)
+    let susps = m.graph.nodes.filter(n => n.original.suspicious != 0)    
+    let all = invs.concat(susps)
+
     return (
         <nav>
-            { m.investigatees.map(m.graph.getnode).map(n => <NodeView n={n} />) }
+            { all.map(n => <NodeView n={n} />) }
         </nav>
     )
 }

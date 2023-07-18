7871233217f8180352217ea8734e854a707a5367
snuup <hop@home.com>
2023-07-18 22:39:08 +0200

    .

--- app/controller.ts
+++ app/controller.ts
@@ -142,8 +142,11 @@ export class Controller {
             m.netgraph.nodes.push(n)
 
             console.warn("add links between", n.id, m.netgraph.nodes)
+
             let paths = GraphAlgos.findpathsmulti(m.supergraph.getlinks, n.id, m.netgraph.nodes.filter(n => n.role == "inv").map(n => n.id).except(n.id))
-            // let paths = GraphAlgos.findpathsmulti(m.supergraph.getlinks, n.id, m.netgraph.nodes.map(n => n.id).except(n.id))
+
+            if (window.all) paths = GraphAlgos.findpathsmulti(m.supergraph.getlinks, n.id, m.netgraph.nodes.map(n => n.id).except(n.id))
+
             console.log("paths", paths.map(p => p.length))
 
             paths.forEach(printpath)
